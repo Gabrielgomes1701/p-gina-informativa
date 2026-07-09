7a14e28c76d61e11ec6d800b9676aef3710d40e7
Gabrielgomes1701 <gomes.souza17@escola.pr.gov.br>
2026-07-09 11:11:55 -0300

    5

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function flipCard(card) {
   if (card.classList.contains("info-card")) return;
   card.classList.toggle("flipped");
 }
+
+function enterSite() {
+  window.location.href = "flashcards.html";
+}
