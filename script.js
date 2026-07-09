8df501cabb5d488cadfeccc235d62c3b358a9dc7
Gabrielgomes1701 <gomes.souza17@escola.pr.gov.br>
2026-07-09 11:43:04 -0300

    15

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+function openCurtain() {
+  document.querySelector('.curtain.left').classList.add('open');
+  document.querySelector('.curtain.right').classList.add('open');
+
+  // Espera a animação terminar antes de ir para a próxima página
+  setTimeout(() => {
+    window.location.href = "flashcards.html";
+  }, 2000);
+}
+
 function flipCard(card) {
   if (card.classList.contains("info-card")) return;
   card.classList.toggle("flipped");
-}
-
-function enterSite() {
-  window.location.href = "flashcards.html";
 }
